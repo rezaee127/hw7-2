75298a5a269d86dcdab92b9c136bfd8dda69539d
razie rezaee <rezaie127@yahoo.com>
2022-02-23 01:00:30 +0330

    landscape

--- app/src/main/java/com/example/hw7_2/Activity2.kt
+++ app/src/main/java/com/example/hw7_2/Activity2.kt
@@ -2,10 +2,18 @@ package com.example.hw7_2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.hw7_2.databinding.Activity2Binding
 
 class Activity2 : AppCompatActivity() {
+    lateinit var binding:Activity2Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_2)
+
+        binding = Activity2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
     }
 }
